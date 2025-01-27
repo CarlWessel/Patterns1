cb2f658f9f3b6177b2ea542f7d7af0a5573d1248
Trish-CS <salamangkittrishia@gmail.com>
2025-01-27 10:21:04 -0500

    StackingStrategy_Pyramid

--- Project1/Start/Strategies/StackingStrategy_Pyramid.cs
+++ Project1/Start/Strategies/StackingStrategy_Pyramid.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Assi1.Aggregates;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Assi1.Strategies
 {
-    internal class StackingStrategy_Pyramid
+    public class StackingStrategy_Pyramid : StackingStrategy
     {
+        public float EvaluateStack(HeavyObjectList input)
+        {
+            float point = 0;
+
+            for (Iterator i = input.CreateIterator(); !i.IsDone(); i.Next())
+            {
+                // See note in StackingStrategy_BottomWeight.cs
+                HeavyObject heavyobj = i.CurrentItem();
+                HeavyObject prevobj = i.GetPreviousItem();
+
+                if (prevobj != null)
+                {
+                    point += (((heavyobj.Width * heavyobj.Length) < (prevobj.Width * prevobj.Length)) ? 1 : -1);
+                }
+            }
+
+            return point;
+        }
     }
 }
